6b61072cb65b44d4ce83bfe58a08575706611fec
unknown <connorjstack.wrk@gmail.com>
2023-12-14 14:29:20 -0600

    adding view

--- src/main/java/lruproject/personal/ClosetManager.java
+++ src/main/java/lruproject/personal/ClosetManager.java
@@ -4,7 +4,7 @@ public class ClosetManager {
     public static void main(String[] args) {
         System.out.println("Hello World from closet!");
         // LOG IN TO AN OWNER
-
+        ClothingMenu.displayMenu();
         // CALL SOME KIND OF VIEW CLASS TO DISPLAY MENU
     }
 }
