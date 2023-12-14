686c3eb293d433e30b7cb1cf6e4e15a3f45942bb
unknown <connorjstack.wrk@gmail.com>
2023-12-14 12:29:01 -0600

    testing branch

--- src/main/java/lruproject/personal/ClosetManager.java
+++ src/main/java/lruproject/personal/ClosetManager.java
@@ -1,7 +1,7 @@
 package lruproject.personal;
+
 public class ClosetManager {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World from closet!" );
+    public static void main(String[] args) {
+        System.out.println("Hello World from closet!");
     }
 }
---
+++ src/main/java/lruproject/personal/ClothingItem.java
@@ -0,0 +1,12 @@
+package lruproject.personal;
+
+public class ClothingItem {
+    private String description = "";
+    private String type = "";
+    private int clothing_id = -1;
+
+    public ClothingItem(String description, String type, int id) {
+        this.description = description;
+    }
+
+}
